8ec4f2a34f39cd6efa034e90e764a98ac497e641
lingxue <xueling794@yeah.net>
2015-01-25 22:45:42 +0800

    add blog ui module

---
+++ web/script/blog/modules/indexApp.js
@@ -0,0 +1,56 @@
+'use strict';
+
+/**
+ * MyApp module
+ */
+
+define(['angular', 'blog/controllers/blogController', 'validate',
+    "main/controllers/headerController", "Util", "DataService", "StateCode"],
+    function (angular, ctrls, validate, headerController, Util, DataService, StateCode) {
+
+
+        angular.module('indexApp', [], function ($compileProvider) {
+
+            $compileProvider.directive('header', function ($compile) {
+                return {
+                    templateUrl: '/script/main/template/header.html',
+                    replace: true,
+                    transclude: false,
+                    restrict: 'E',
+                    controller: function ($scope) {
+                        //$scope.loginFlag =true;
+                        headerController.header($scope);
+                    },
+                    scope: false
+                };
+            });
+            $compileProvider.directive('footer', function ($compile) {
+                return {
+                    templateUrl: '/script/main/template/footer.html',
+                    replace: true,
+                    transclude: false,
+                    restrict: 'E',
+                    controller: function ($scope) {
+
+                    },
+                    scope: false
+                };
+            });
+            $compileProvider.directive('container', function ($compile) {
+                return {
+                    templateUrl: '/script/blog/template/index.html',
+                    replace: true,
+                    transclude: false,
+                    restrict: 'E',
+                    controller: function ($scope) {
+
+                    },
+                    scope: false
+                };
+            });
+
+
+        });
+
+
+    });
